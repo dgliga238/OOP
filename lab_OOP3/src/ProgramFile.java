f23e470509f883577c0cb52aa908f28dae89afe2
Daniela Gliga <daniela.gliga@isa.utm.md>
2023-10-27 14:18:45 +0300

    ProgramFile Class added

--- lab_OOP3/src/ProgramFile.java
+++ lab_OOP3/src/ProgramFile.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class ProgramFile {
+class ProgramFile extends File {
+    private int lineCount;
+    private int classCount;
+    private int methodCount;
+
+    public ProgramFile(String name, String extension, long createdTime, long updatedTime, String content) {
+        super(name, extension, createdTime, updatedTime);
+
+        String[] lines = content.split("\n");
+        this.lineCount = lines.length;
+        this.classCount = 0;
+        this.methodCount = 0;
+
+        if (extension.equals(".py")) {
+            for (String line : lines) {
+                if (line.trim().startsWith("class ")) {
+                    this.classCount++;
+                } else if (line.trim().startsWith("def ")) {
+                    this.methodCount++;
+                }
+            }
+        } else if (extension.equals(".java")) {
+            for (String line : lines) {
+                if (line.trim().startsWith("class ") || line.trim().startsWith("interface ")) {
+                    this.classCount++;
+                } else if (line.trim().startsWith("public ") || line.trim().startsWith("private ")) {
+                    this.methodCount++;
+                }
+            }
+        }
+    }
 }
