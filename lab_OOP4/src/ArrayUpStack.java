ef9d319aeb3706078e3f8e52053214b495e98df3
Daniela Gliga <daniela.gliga@isa.utm.md>
2023-11-27 13:24:33 +0200

    add implementation

--- lab_OOP4/src/ArrayUpStack.java
+++ lab_OOP4/src/ArrayUpStack.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class ArrayUpStack {
+public class ArrayUpStack<T> implements Stack<T> {
+    private final int maxSize;
+    private final Class<T> dataType;
+    private Object[] data;
+    private int size;
+
+    public ArrayUpStack(int maxSize, Class<T> dataType) {
+        this.maxSize = maxSize;
+        this.dataType = dataType;
+        this.data = new Object[maxSize];
+        this.size = 0;
+    }
+
+    @Override
+    public void push(T element) {
+        if (!dataType.isInstance(element)) {
+            throw new IllegalArgumentException("Element type is not the same as the specified data type");
+        }
+        if (size == maxSize) {
+            throw new IllegalStateException("Stack is full");
+        }
+        data[size++] = element;
+    }
+
+    @Override
+    public T pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return dataType.cast(data[--size]);
+    }
+
+    @Override
+    public T element() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return dataType.cast(data[size - 1]);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean isFull() {
+        return size == maxSize;
+    }
 }
--- lab_OOP4/src/LinkedQueue.java
+++ lab_OOP4/src/LinkedQueue.java
@@ -1,2 +1,74 @@
-package PACKAGE_NAME;public class LinkedQueue {
+public class LinkedQueue<T> implements Queue<T> {
+    private static class Node<T> {
+        T data;
+        Node<T> next;;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node<T> front, rear;
+    private int size;
+    private final int maxSize = 5;
+
+    public LinkedQueue(int i, Class<Integer> integerClass) {
+        front = null;
+        rear = null;
+        size = 0;
+    }
+
+    @Override
+    public void enqueue(T element) {
+        if (!isFull()) {
+            Node<T> newNode = new Node<>(element);
+            if (isEmpty()) {
+                front = newNode;
+            } else {
+                rear.next = newNode;
+            }
+            rear = newNode;
+            size++;
+        } else {
+            System.out.println("Queue is full. Cannot enqueue element: " + element);
+        }
+    }
+
+    @Override
+    public T dequeue() {
+        if (!isEmpty()) {
+            T data = front.data;
+            front = front.next;
+            if (front == null) {
+                rear = null;
+            }
+            size--;
+            return data;
+        } else {
+            System.out.println("Queue is empty. Cannot dequeue element.");
+            return null;
+        }
+    }
+
+    @Override
+    public T peek() {
+        if (!isEmpty()) {
+            return front.data;
+        } else {
+            System.out.println("Queue is empty. Cannot peek.");
+            return null;
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean isFull() {
+        return size == maxSize;
+    }
 }
+
